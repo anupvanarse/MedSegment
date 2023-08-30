bec697ad9fec4a8428fbbf41e8dbdd6c2c1863fe
Anup Vanarse <avanarse@brainchipinc.com>
2023-08-30 13:10:38 +0800

    Basic structure changes

--- train.py
+++ train.py
@@ -1,93 +1,91 @@
-#
-# -*- coding: utf-8 -*-
-#
-# Copyright (c) 2019 Intel Corporation
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-# SPDX-License-Identifier: EPL-2.0
-#
-
-"""
-This module loads the data from data.py, creates a TensorFlow/Keras model
-from model.py, trains the model on the data, and then saves the
-best model.
-"""
-
 import datetime
 import os
-
+import argparse
 import tensorflow as tf  # conda install -c anaconda tensorflow
 import settings   # Use the custom settings.py file for default parameters
 
-from dataloader import DatasetGenerator, get_decathlon_filelist
+from utils.dataloader import DatasetGenerator, get_decathlon_filelist
 
 import numpy as np
 
-from argparser import args
-
-"""
-For best CPU speed set the number of intra and inter threads
-to take advantage of multi-core systems.
-See https://github.com/intel/mkl-dnn
-"""
-
-os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"  # Get rid of the AVX, SSE warnings
-
-# If hyperthreading is enabled, then use
-os.environ["KMP_AFFINITY"] = "granularity=thread,compact,1,0"
-
-# If hyperthreading is NOT enabled, then use
-#os.environ["KMP_AFFINITY"] = "granularity=thread,compact"
-
-os.environ["KMP_BLOCKTIME"] = str(args.blocktime)
-
-os.environ["OMP_NUM_THREADS"] = str(args.num_threads)
-os.environ["INTRA_THREADS"] = str(args.num_threads)
-os.environ["INTER_THREADS"] = str(args.num_inter_threads)
-os.environ["KMP_SETTINGS"] = "0"  # Show the settings at runtime
-
-def test_intel_tensorflow():
-    """
-    Check if Intel version of TensorFlow is installed
-    """
-    import tensorflow as tf
-
-    print("We are using Tensorflow version {}".format(tf.__version__))
-
-    major_version = int(tf.__version__.split(".")[0])
-    if major_version >= 2:
-        from tensorflow.python import _pywrap_util_port
-        print("Intel-optimizations (DNNL) enabled:",
-              _pywrap_util_port.IsMklEnabled())
-    else:
-        print("Intel-optimizations (DNNL) enabled:",
-              tf.pywrap_tensorflow.IsMklEnabled())
 
 if __name__ == "__main__":
-
-    START_TIME = datetime.datetime.now()
-    print("Started script on {}".format(START_TIME))
-
-    print("Runtime arguments = {}".format(args))
-    test_intel_tensorflow() # Print if we are using Intel-optimized TensorFlow
-
     """
     Create a model, load the data, and train it.
     """
+    """
+    Step 1: Parse command line arguments
+    """
+    parser = argparse.ArgumentParser(
+    description="2D U-Net model (Keras-Core) on BraTS Decathlon dataset.",
+    add_help=True, formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument("--data_path", default=settings.DATA_PATH,
+                        help="The path to the Medical Decathlon directory")
+    parser.add_argument("--output_path", default=settings.OUT_PATH,
+                        help="the folder to save the model and checkpoints")
+    parser.add_argument("--inference_filename", default=settings.INFERENCE_FILENAME,
+                        help="the Keras inference model filename")
+    parser.add_argument("--use_upsampling",
+                        help="use upsampling instead of transposed convolution",
+                        action="store_true", default=settings.USE_UPSAMPLING)
+    parser.add_argument("--num_threads", type=int,
+                        default=settings.NUM_INTRA_THREADS,
+                        help="the number of threads")
+    parser.add_argument("--num_inter_threads", type=int,
+                        default=settings.NUM_INTER_THREADS,
+                        help="the number of intraop threads")
+    parser.add_argument("--batch_size", type=int, default=settings.BATCH_SIZE,
+                        help="the batch size for training")
+    parser.add_argument("--split", type=float, default=settings.TRAIN_TEST_SPLIT,
+                        help="Train/testing split for the data")
+    parser.add_argument("--seed", type=int, default=settings.SEED,
+                        help="Seed for random number generation")
+    parser.add_argument("--crop_dim", type=int, default=settings.CROP_DIM,
+                        help="Size to crop images (square, in pixels). If -1, then no cropping.")
+    parser.add_argument("--blocktime", type=int,
+                        default=settings.BLOCKTIME,
+                        help="blocktime")
+    parser.add_argument("--epochs", type=int,
+                        default=settings.EPOCHS,
+                        help="number of epochs to train")
+    parser.add_argument("--learningrate", type=float,
+                        default=settings.LEARNING_RATE,
+                        help="learningrate")
+    parser.add_argument("--weight_dice_loss", type=float,
+                        default=settings.WEIGHT_DICE_LOSS,
+                        help="Weight for the Dice loss compared to crossentropy")
+    parser.add_argument("--featuremaps", type=int,
+                        default=settings.FEATURE_MAPS,
+                        help="How many feature maps in the model.")
+    parser.add_argument("--use_pconv", help="use partial convolution based padding",
+                        action="store_true",
+                        default=settings.USE_PCONV)
+    parser.add_argument("--channels_first", help="use channels first data format",
+                        action="store_true", default=settings.CHANNELS_FIRST)
+    parser.add_argument("--print_model", help="print the model",
+                        action="store_true",
+                        default=settings.PRINT_MODEL)
+    parser.add_argument("--use_dropout",
+                        default=settings.USE_DROPOUT,
+                        help="add spatial dropout layers 3/4",
+                        action="store_true",
+                        )
+    parser.add_argument("--use_augmentation",
+                        default=settings.USE_AUGMENTATION,
+                        help="use data augmentation on training images",
+                        action="store_true")
+    parser.add_argument("--output_pngs",
+                        default="inference_examples",
+                        help="the directory for the output prediction pngs")
+    parser.add_argument("--input_filename",
+                        help="Name of saved TensorFlow model directory",
+                        default=os.path.join(settings.OUT_PATH,settings.INFERENCE_FILENAME))
+
+    args = parser.parse_args()
 
     """
-    Step 1: Define a data loader
+    Step 2: Define a data loader
     """
     print("-" * 30)
     print("Loading the data from the Medical Decathlon directory to a TensorFlow data loader ...")
@@ -104,12 +102,10 @@ if __name__ == "__main__":
     print("-" * 30)
 
     """
-    Step 2: Define the model
+    Step 3: Define the model
     """
-    if args.use_pconv:
-        from model_pconv import unet
-    else:
-        from model import unet
+
+    from utils.model import unet
 
     unet_model = unet(channels_first=args.channels_first,
                  fms=args.featuremaps,
@@ -127,7 +123,7 @@ if __name__ == "__main__":
     model_filename, model_callbacks = unet_model.get_callbacks()
 
     """
-    Step 3: Train the model on the data
+    Step 4: Train the model on the data
     """
     print("-" * 30)
     print("Fitting model with training data ...")
@@ -140,7 +136,7 @@ if __name__ == "__main__":
               callbacks=model_callbacks)
 
     """
-    Step 4: Evaluate the best model
+    Step 5: Evaluate the best model
     """
     print("-" * 30)
     print("Loading the best trained model ...")
@@ -149,15 +145,15 @@ if __name__ == "__main__":
     unet_model.evaluate_model(model_filename, ds_test)
 
     """
-    Step 5: Print the command to convert TensorFlow model into OpenVINO format with model optimizer.
+    Step 6: Print the command to convert TensorFlow model into OpenVINO format with model optimizer.
     """
-    print("-" * 30)
-    print("-" * 30)
-    unet_model.print_openvino_mo_command(
-        model_filename, ds_test.get_input_shape())
+    # print("-" * 30)
+    # print("-" * 30)
+    # unet_model.print_openvino_mo_command(
+    #     model_filename, ds_test.get_input_shape())
 
-    print(
-        "Total time elapsed for program = {} seconds".format(
-            datetime.datetime.now() -
-            START_TIME))
-    print("Stopped script on {}".format(datetime.datetime.now()))
+    # print(
+    #     "Total time elapsed for program = {} seconds".format(
+    #         datetime.datetime.now() -
+    #         START_TIME))
+    # print("Stopped script on {}".format(datetime.datetime.now()))
